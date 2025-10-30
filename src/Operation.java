213cf03be72b2c0517397880874cd1e6e0bbae9b
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-10-30 15:15:16 +0100

    feat: methods triangle and repeat

--- src/Operation.java
+++ src/Operation.java
@@ -18,10 +18,34 @@ class Operation {
 		return nb1/nb2;
 	}
 	
+	// 6.2 -----------------------------------------------------------------------------
+
+	public static void repeat(int nbRepeat, String symbole) {
+		// Répéter un symbole x fois
+		for(int j = 1; j <= nbRepeat ; j++) {
+				System.out.print(symbole);
+			}
+	}
+	
+	public static void triangle(int nbStars) {
+		int positionTriangle = nbStars/2;
+		for(int i = 1; i <= nbStars; i+=2) {
+			
+			repeat(positionTriangle-=1," ");
+			repeat(i,"*");
+			System.out.println("");
+		}
+		
+	}
+	
+	//-----------------------------------------------------------------------------
+	
 	public static void main(String[] args) {
 		System.out.println(addition(5,2));
 		System.out.println(substract(5,2));
 		System.out.println(multiplication(5,2));
 		System.out.println(division(3,0));
+		
+		triangle(20);
 	}
 }
